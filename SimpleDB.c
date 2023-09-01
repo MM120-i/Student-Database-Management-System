d4b72b384f6b065c3038c4b2b200be82eacc81e4
Mahim <80307451+MM120-i@users.noreply.github.com>
2023-09-01 15:11:46 -0400

    Update SimpleDB.c

--- SimpleDB.c
+++ SimpleDB.c
@@ -12,19 +12,20 @@ struct student{
 
     char first_name[20];
     char last_name[20];
-    int roll_no;
+    int gpa;
     char grade[10];
     char city[20];
     float per;
 };
 
 
-void main(){
-
+void main() {
     int choice = 0;
 
-    while(choice != 5){
+    // Main loop to display the menu and process user choices
+    while (choice != 5) {  // Continue until the user chooses to exit (option 5)
 
+        // Display the main menu
         printf("\n\t\t\t==========Student Database Management System==========");
         printf("\n\n\n\t\t\t\t 1. Add Student Record\n");
         printf("\t\t\t\t 2. Student Records\n");
@@ -33,201 +34,223 @@ void main(){
         printf("\t\t\t\t 5. Exit\n");
         printf("\n\t\t\t ___________________________________________________________\n");
         printf("\t\t\t\t");
+
+        // Read the user's choice
         scanf("%d", &choice);
 
-        switch (choice){
+        switch (choice) {
 
-        case 1:
-            
-            system("cls");
-            addStudent();
-            system("cls");
-            break;
+            case 1:
 
-        case 2:
+                system("cls");  // Clear the screen
+                addStudent();   // Call the function to add a student record
+                system("cls");  // Clear the screen again
+                break;
 
-            system("cls");
-            studentRecord();
-            printf("\t\t\tPress any key to exit\n");    
-            getch();
-            system("cls");
-            break;
-        
-        case 3:
+            case 2:
 
-            system("cls");
-            searchStudent();
-            printf("\n\t\t\t\tPress any key to exit\n");
-            getch();
-            system("cls");
-            break;
+                system("cls");        // Clear the screen
+                studentRecord();      // Call the function to display student records
+                printf("\t\t\tPress any key to exit\n");
+                getch();              // Wait for a key press
+                system("cls");        // Clear the screen again
+                break;
 
-        case 4:
+            case 3:
 
-            system("cls");
-            delete();
-            printf("\n\t\t\t\tPress any key to exit\n");
-            getch();
-            system("cls");
-            break;
-        
-        case 5:
-        
-            exit(0);
-            break;
-        
-        default:
+                system("cls");        // Clear the screen
+                searchStudent();      // Call the function to search for a student record
+                printf("\n\t\t\t\tPress any key to exit\n");
+                getch();              // Wait for a key press
+                system("cls");        // Clear the screen again
+                break;
 
-            printf("\n\t\t\t\tPlease select a valid option (1-5)");
-            break;
+            case 4:
+
+                system("cls");  // Clear the screen
+                delete(); // Call the function to delete a student record
+                printf("\n\t\t\t\tPress any key to exit\n");
+                getch();        // Wait for a key press
+                system("cls");  // Clear the screen again
+                break;
+
+            case 5:
+
+                exit(0);  // Exit the program
+                break;
+
+            default:
+
+                printf("\n\t\t\t\tPlease select a valid option (1-5)\n");
+                break;
         }
     }
 }
 
-void addStudent(){
+
+void addStudent() {
 
     char another;
     FILE *fp;
     struct student info;
 
-    do{
+    do {
 
-        system("cls");
-        printf("\t\t\t\t==========Add Student Infomation==========\n\n\n");
-        fp = fopen("studentInfo", "a");
+        system("cls");  // Clear the screen
+        printf("\t\t\t\t==========Add Student Information==========\n\n\n");
+        fp = fopen("studentInfo", "a");  // Open the file for appending
 
+        // Input student information
         printf("\n\t\t\tEnter first name   : ");
         scanf("%s", &info.first_name); 
 
         printf("\n\t\t\tEnter last name    : ");
         scanf("%s", &info.last_name);
 
-        printf("\n\t\t\tEnter roll number  : ");
-        scanf("%d", &info.roll_no);
+        printf("\n\t\t\tEnter GPA         : ");
+        scanf("%d", &info.gpa);
 
-        printf("\n\t\t\tEnter grade        : ");
+        printf("\n\t\t\tEnter grade       : ");
         scanf("%s", &info.grade);
 
-        printf("\n\t\t\tEnter address      : ");
+        printf("\n\t\t\tEnter address     : ");
         scanf("%s", &info.city);
 
-        printf("\n\t\t\tEnter percentage   : ");
+        printf("\n\t\t\tEnter percentage  : ");
         scanf("%f", &info.per);
 
         printf("\n\t\t\t___________________________________\n");
 
-        if(fp == NULL){
-            fprintf(stderr, "\t\t\tCannot open file\n");
-        }
-        else{
-            printf("\t\t\tRecord stored successfully");
+        // Check if the file was opened successfully
+        if (fp == NULL) {
+            
+            fprintf(stderr, "\t\t\tCannot open file\n");  // Display an error message
+        } 
+        else {
+
+            printf("\t\t\tRecord stored successfully");  // Display a success message
         }
 
-        fwrite(&info,sizeof(struct student), 1, fp);
-        fclose(fp);
+        // Write the student information to the file
+        fwrite(&info, sizeof(struct student), 1, fp);
+        fclose(fp);  // Close the file
 
+        // Prompt to add another record
         printf("\t\t\t\nDo you want to add another record? (Y/N): ");
         scanf("%s", &another);
-        another = toupper(another);
-        
-    } while (another == 'Y');
-    
+        another = toupper(another);  // Convert the response to uppercase
+
+    } while (another == 'Y');  // Repeat if the user wants to add another record
 }
 
-void studentRecord(){
+
+void studentRecord() {
 
     FILE *fp;
     struct student info;
-    fp = fopen("studentInfo", "r");
+    fp = fopen("studentInfo", "r");  // Open the file for reading
 
     printf("\t\t\t\t========== Student Records ==========\n\n\n");
 
-    if(fp == NULL){
-        fprintf(stderr, "\t\t\tCannot open\n");
-    }
-    else{
+    // Check if the file was opened successfully
+    if (fp == NULL) {
+
+        fprintf(stderr, "\t\t\tCannot open\n");  // Display an error message
+    } else {
+
         printf("\t\t\t\tRecords\n");
         printf("\t\t\t\t__________________\n\n");
     }
 
-    while(fread(&info, sizeof(struct student), 1, fp)){
-        
+    while (fread(&info, sizeof(struct student), 1, fp)) {
+
+        // Display student information
         printf("\n\t\t\t\tStudent name   : %s %s", info.first_name, info.last_name);
-        printf("\n\t\t\t\tRoll no        : %d", info.roll_no);
+        printf("\n\t\t\t\tGPA            : %d", info.gpa);
         printf("\n\t\t\t\tGrade          : %s", info.grade);
-        printf("\n\t\t\t\tAdderess       : %s", info.city);
+        printf("\n\t\t\t\tAddress        : %s", info.city);
         printf("\n\t\t\t\tPercentage     : %0.2f", info.per);
         printf("\n\t\t\t\t_____________________________________________\n");
     }
 
-    fclose(fp);
-    getch();
+    fclose(fp);  // Close the file
+    getch();     // Wait for a key press before returning
 }
 
-void searchStudent(){
+
+void searchStudent() {
 
     FILE *fp;
     struct student info;
-    int roll_no, found = 0;
-    fp = fopen("studentInfo", "r");
+    int gpa, found = 0;
+    fp = fopen("studentInfo", "r");  // Open the file for reading
 
     printf("\t\t\t\t========== Search Student ==========\n\n\n");
-    printf("\t\t\tEnter roll no: ");
-    scanf("%d", &roll_no);
+    printf("\t\t\tEnter GPA: ");
+    scanf("%d", &gpa);
 
-    while(fread(&info, sizeof(struct student), 1, fp)){
+    while (fread(&info, sizeof(struct student), 1, fp)) {
 
-        if(info.roll_no == roll_no){
+        // Check if the GPA matches the search criteria
+        if (info.gpa == gpa) {
 
             found = 1;
+            // Display student information
             printf("\n\t\t\t\tStudent name   : %s %s", info.first_name, info.last_name);
-            printf("\n\t\t\t\tRoll no        : %d", info.roll_no);
+            printf("\n\t\t\t\tGPA            : %d", info.gpa);
             printf("\n\t\t\t\tGrade          : %s", info.grade);
-            printf("\n\t\t\t\tAdderess       : %s", info.city);
+            printf("\n\t\t\t\tAddress        : %s", info.city);
             printf("\n\t\t\t\tPercentage     : %0.2f", info.per);
             printf("\n\t\t\t\t_____________________________________________\n");
         }
     }
 
-    if(!found){
-        
+    if (!found) {
+
+        // Display a message if no matching records were found
         printf("\t\t\tRecord not found\n");
     }
 
-    fclose(fp);
-    getch();
+    fclose(fp);  // Close the file
+    getch();     // Wait for a key press before returning
 }
+
 
 void delete() {
 
     FILE *fp, *fp1;
     struct student info;
-    int roll_no, found = 0;
+    int gpa, found = 0;
 
     printf("\t\t\t\t========== Delete Student Record ==========\n\n\n");
-    printf("\t\t\tEnter roll no: ");
-    scanf("%d", &roll_no);
+    printf("\t\t\tEnter GPA: ");
+    scanf("%d", &gpa);
 
-    fp = fopen("studentInfo", "r");
+    fp = fopen("studentInfo", "r"); // Open the file for reading
     if (fp == NULL) {
 
         fprintf(stderr, "\t\t\t\tCan't open file\n");
         return;
     }
 
-    fp1 = fopen("temp.txt", "w");
+    fp1 = fopen("temp.txt", "w"); // Open a temporary file for writing
     if (fp1 == NULL) {
 
         fprintf(stderr, "\t\t\t\tCan't open temporary file\n");
-        fclose(fp); // Close the file before returning
+        fclose(fp); // Close the original file before returning
         return;
     }
 
     while (fread(&info, sizeof(struct student), 1, fp)) {
 
-        if (info.roll_no == roll_no) {
+        // Check if the GPA matches the search criteria
+        if (info.gpa == gpa) {
+
             found = 1;
-        } else {
+        } 
+        else {
+
+            // Write records that don't match the search criteria to the temporary file
             fwrite(&info, sizeof(struct student), 1, fp1);
         }
     }
@@ -237,12 +260,15 @@ void delete() {
 
     if (found) {
 
+        // Delete the original file and rename the temporary file to the original file's name
         remove("studentInfo");
         rename("temp.txt", "studentInfo");
         printf("\n\t\t\tRecord deleted successfully\n");
     } else {
+
         printf("\n\t\t\tRecord not found\n");
     }
 
-    getch();
+    getch(); // Wait for a key press before returning
 }
+
